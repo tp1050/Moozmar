e125b72872d9a4220f0901c5f4561e494f8ab369
c1 <tp1050@gmail.com>
2021-11-10 00:02:27 +0330

    01-2

--- bonbast.py
+++ bonbast.py
@@ -1,7 +1,13 @@
 from Price import *
 
 class bonbastPrice(Price):
-    __init__:
+    def __init__(self,value=0,vaght=datetime.now(),jens='dool',conn='No'):
         self.refCurID=49
         self.refCur='fx_irr'
+        self.src='bonbast'
+        self.vaght=vaght
+        self.value=value
+        self.jens=jens
+
+
 
